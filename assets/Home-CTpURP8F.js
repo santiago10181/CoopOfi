93af1322accd5875fef81c4bcde2e9a834bd4f55
Santiago Diaz <santiago.marino.diaz@gmail.com>
2026-02-16 17:06:46 -0500

    Updates

--- assets/Home-CTpURP8F.js
+++ assets/Home-CTpURP8F.js
@@ -1,4 +1,4 @@
-import{n as e,u as t}from"./index-D9wT8T76.js";var n=t(e(),1);const r=({children:e,className:t=``})=>(0,n.jsx)(`div`,{className:`
+import{n as e,u as t}from"./index-CHeRPjSq.js";var n=t(e(),1);const r=({children:e,className:t=``})=>(0,n.jsx)(`div`,{className:`
         bg-black/5
         rounded-2xl 
         p-6 
